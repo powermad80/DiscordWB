96b8c6bb1a12a128c90d5803fe552bb26cb180bf
Jake <Jake@DESKTOP-1VSHC3B>
2016-11-10 21:49:51 -0500

    Added #fortune among others

--- botscript/Program.cs
+++ botscript/Program.cs
@@ -8,6 +8,7 @@ class Program
     public void Start()
     {
         _client = new DiscordClient();
+        System.Random random = new System.Random();
         _client.MessageReceived += async (s, e) =>
         {
             if (e.Message.RawText == "Welcome Karen")
@@ -18,7 +19,28 @@ class Program
             if (e.Message.User.ToString() == "Accel#4471")
                 if (e.Message.RawText == "Karen!")
                     await e.Channel.SendMessage("*slaps Moldy*");
-
+            if (e.Message.RawText == "Say hi, Karen")
+                await e.Channel.SendMessage("Hello!");
+            if (e.Message.RawText == "#fortune")
+            {
+                int fortune = random.Next(0, 101);
+                if (fortune >= 95)
+                    await e.Channel.SendMessage("[大吉] Great Blessing");
+                else if (fortune >= 70)
+                    await e.Channel.SendMessage("[中吉] Middle Blessing");
+                else if (fortune >= 55)
+                    await e.Channel.SendMessage("[吉] Blessing");
+                else if (fortune >= 30)
+                    await e.Channel.SendMessage("[小吉] Small Blessing");
+                else if (fortune >= 17)
+                    await e.Channel.SendMessage("[小凶] Small Curse");
+                else if (fortune >= 8)
+                    await e.Channel.SendMessage("[半凶] Half-curse");
+                else if (fortune >= 3)
+                    await e.Channel.SendMessage("[凶] Curse");
+                else if (fortune >= 0)
+                    await e.Channel.SendMessage("[大凶] Great Curse");
+            }
         };
         _client.ExecuteAndWait(async () =>
         {
